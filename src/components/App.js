dcf0e7f6c56b8a65f4380c2c72847602ebfa31e4
jorgebaralt <jorgebaraltq@gmaik.com>
2018-05-02 23:42:06 -0400

    main menu/ sider/ header changes

--- src/components/App.js
+++ src/components/App.js
@@ -5,16 +5,27 @@ import CarServices from './CarServices';
 import OilChange from './OilChange';
 import {selectMenuOption} from "../actions";
 import {connect} from 'react-redux';
-import { Layout, Menu, Icon, Button } from 'antd';
+import { Layout, Menu, Icon, Button, BackTop,Row, Col } from 'antd';
 
 const { Header, Content, Footer, Sider } = Layout;
 const {SubMenu} = Menu;
 
 
 class App extends Component{
+    // react state to handle the sider collapse
+    state={
+        collapse:false
+    };
+    toggle = () => {
+        this.setState({
+            collapsed: !this.state.collapsed,
+        });
+    };
+
     onMenuClicked({key}){
         this.props.selectMenuOption(key);
     }
+
     renderContent(){
         const {selectedMenu} = this.props;
         switch (selectedMenu){
@@ -27,16 +38,30 @@ class App extends Component{
             default:
                 return(<div>Please Select from Menu</div>);
         }
-
     }
 
     render(){
         const {siderStyle,headerStyle,contentStyle,layoutStyle,footerStyle} = styles;
+
       return(
+
           <Layout>
-              <Sider className="sider" style={siderStyle}>
+              <div>
+                  <BackTop visibilityHeight={100}>
+                      <div className="ant-back-top-inner">UP</div>
+                  </BackTop>
+
+              </div>
+              <Sider
+                  trigger={null}
+                  collapsible
+                  collapsed={this.state.collapsed}
+                  className="sider"
+                  style={siderStyle}
+              >
+
                   {/*TODO : maybe logo here*/}
-                  <div className="logo">Tire Outlet Logo</div>
+                  <div className="logo">Tire Outlet</div>
 
                   <Menu className="menu" mode="inline" onClick={this.onMenuClicked.bind(this)} defaultSelectedKeys={['home']} >
                       <Menu.Item key="home">
@@ -50,21 +75,24 @@ class App extends Component{
                           <Menu.Item key="oilChange"><Icon type="filter"/>Oil Change</Menu.Item>
                           <Menu.Item key="carServices"><Icon type="sync"/>Car Services</Menu.Item>
                       </SubMenu>
-
                   </Menu>
               </Sider>
 
               <Layout style={layoutStyle}>
-
                   <Header style={headerStyle}>
-                      <div>
+                    <Row>
+                          <Icon
+                              className="trigger"
+                              type={this.state.collapsed ? 'menu-unfold' : 'menu-fold'}
+                              onClick={this.toggle}
+                          />
                           <span id={"header-title"}>Tire Outlet Truck Bus & Car</span>
                           <span id={"header-login"}> <Button>Admin Login</Button></span>
-                      </div>
+                    </Row>
                   </Header>
 
                   <Content style={contentStyle}>
-                      <div style={{ padding: 24, background: '#fff', textAlign: 'center' }}>
+                      <div>
                           {this.renderContent()}
                           ...
                           <br />
@@ -92,24 +120,21 @@ class App extends Component{
 const styles={
     siderStyle:{
         overflow: 'auto',
-        height: '100vh',
-        position: 'fixed',
-        left: 0
-    },
-    layoutStyle:{
-        marginLeft: 200
+        left: 0,
     },
     headerStyle:{
         background: '#fff',
         padding: 0
     },
     contentStyle:{
-        margin: '24px 16px 0',
-        overflow: 'initial'
+        margin: '24px 16px',
+        padding: 24,
+        background:'#fff',
+        textAlign: 'center'
     },
     footerStyle:{
         textAlign: 'center'
-    }
+    },
 };
 
 function mapStateToProps(state){
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,21 @@
 import React, {Component} from 'react'
+import {Carousel} from 'antd'
 class Home extends Component{
+
     render(){
-        return(<div>This is home!</div>)
+        return(
+            <div>
+                <div id={"home-carousel"}>
+                    <Carousel autoplay>
+                        <div><h2>1</h2></div>
+                        <div><h2>2</h2></div>
+                        <div><h2>3</h2></div>
+                    </Carousel>
+                </div>
+            </div>
+
+        )
+
     };
 }
 
